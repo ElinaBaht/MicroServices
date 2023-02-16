a7d46e8c712ae9ff9c4f25759f04abdd4dda87b6
Elina <elina.bahtegaryaeva@gmail.com>
2023-02-16 18:01:40 +0300

    added repositories

---
+++ Services.ProductAPI/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.ProductAPI.Models;
+
+namespace Services.ProductAPI.DBContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Services.ProductAPI.Models.Dto;
+
+namespace Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto product);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
